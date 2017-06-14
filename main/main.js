42a2717a2ce64c4be38fa1381838071a37ad7344
carons-cr <2738794789@qq.com>
2017-06-14 22:29:11 +0800

    把'改成了"，连换行都和要求放一样的位置上

--- main/main.js
+++ main/main.js
@@ -6,59 +6,61 @@ module.exports = function main(s) {
     	 var outArray=new Array();
     	 outArray=figure();
     	 var resultArray=new Array();
-    	 resultArray[0]='';
-    	 resultArray[1]='';
-    	 resultArray[2]='';
+    	 resultArray[0]="";
+    	 resultArray[1]="";
+    	 resultArray[2]="";
     	 for(var i=0;i<inArray.length;i++){
-	         for(var j=0;j<3;j++){
-	    	        resultArray[j]+=outArray[inArray[i]][j]+'\t';
-	         }      
+	    for(var j=0;j<3;j++){
+	    	resultArray[j]+=outArray[inArray[i]][j]+"\t";
+	    }      
     	 }
-    	 var result=resultArray[0]+'\n'+resultArray[1]+'\n'+resultArray[2];
+    	 var result=resultArray[0]+"\n"
+             +resultArray[1]+"\n"
+             +resultArray[2];
     	 return result;
-};
+     }
 
 function figure(){
     	 var outArray=new Array();
 	     outArray[0]=new Array();
-    	     outArray[0][0]='._.';
-    	     outArray[0][1]='|.|';
-    	     outArray[0][2]='|_|';
+    	     outArray[0][0]="._.";
+    	     outArray[0][1]="|.|";
+    	     outArray[0][2]="|_|";
 	     outArray[1]=new Array();
-    	     outArray[1][0]='...';
-    	     outArray[1][1]='..|';
-    	     outArray[1][2]='..|';
+    	     outArray[1][0]="...";
+    	     outArray[1][1]="..|";
+    	     outArray[1][2]="..|";
     	 outArray[2]=new Array();
-    	     outArray[2][0]='._.';
-    	     outArray[2][1]='._|';
-    	     outArray[2][2]='|_.';
+    	     outArray[2][0]="._.";
+    	     outArray[2][1]="._|";
+    	     outArray[2][2]="|_.";
     	 outArray[3]=new Array();
-    	     outArray[3][0]='._.';
-    	     outArray[3][1]='._|';
-    	     outArray[3][2]='._|';
+    	     outArray[3][0]="._.";
+    	     outArray[3][1]="._|";
+    	     outArray[3][2]="._|";
     	 outArray[4]=new Array();
-    	     outArray[4][0]='...';
-    	     outArray[4][1]='|_|';
-    	     outArray[4][2]='..|';
+    	     outArray[4][0]="...";
+    	     outArray[4][1]="|_|";
+    	     outArray[4][2]="..|";
     	 outArray[5]=new Array();
-    	     outArray[5][0]='._.';
-    	     outArray[5][1]='|_.';
-    	     outArray[5][2]='._|';
+    	     outArray[5][0]="._.";
+    	     outArray[5][1]="|_.";
+    	     outArray[5][2]="._|";
     	 outArray[6]=new Array();
-    	     outArray[6][0]='._.';
-    	     outArray[6][1]='|_.';
-    	     outArray[6][2]='|_|';
+    	     outArray[6][0]="._.";
+    	     outArray[6][1]="|_.";
+    	     outArray[6][2]="|_|";
     	 outArray[7]=new Array();
-    	     outArray[7][0]='._.';
-    	     outArray[7][1]='..|';
-    	     outArray[7][2]='..|';
+    	     outArray[7][0]="._.";
+    	     outArray[7][1]="..|";
+    	     outArray[7][2]="..|";
     	 outArray[8]=new Array();
-    	     outArray[8][0]='._.';
-    	     outArray[8][1]='|_|';
-    	     outArray[8][2]='|_|';
+    	     outArray[8][0]="._.";
+    	     outArray[8][1]="|_|";
+    	     outArray[8][2]="|_|";
     	 outArray[9]=new Array();
-    	     outArray[9][0]='._.';
-    	     outArray[9][1]='|_|';
-    	     outArray[9][2]='..|';
+    	     outArray[9][0]="._.";
+    	     outArray[9][1]="|_|";
+    	     outArray[9][2]="..|";
     	 return outArray;
      }
